d41ae21e36aa786d44cf5b7cee593cf361d8dfd9
Vikash Ramdarash Chaurasiya <vikashch97600@gmail.com>
2026-05-06 21:18:29 +0530

    Fixed some issues

--- panchayat/settings.py
+++ panchayat/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
